53877846639113d3cbc0eb7dac373453dd5cd0f3
Ulrich Germann <ulrich.germann@gmail.com>
2021-03-16 13:34:47 +0000

    Re-establish compilation with abseil.
    This is mostly to enable compilation with C++11 where C++17 is not available.

--- src/ssplit/typedefs.h
+++ src/ssplit/typedefs.h
@@ -1,17 +1,20 @@
 #pragma once
-// #include "absl/strings/string_view.h"
-// #include "absl/container/flat_hash_map.h"
 #include <string>
-#include <unordered_map>
-#include <map>
+#if USE_ABSEIL
+#  include "absl/strings/string_view.h"
+#  include "absl/container/flat_hash_map.h"
+#else
+#  include <map>
+#endif
 
 namespace ug{
 namespace ssplit{
 
-// typedef absl::string_view string_view;
-// typedef absl::flat_hash_map<std::string, int> prefix_map_t;
-
-typedef std::string_view string_view;
-typedef std::map<std::string, int, std::less<> > prefix_map_t;
-
+#if USE_ABSEIL
+  typedef absl::string_view string_view;
+  typedef absl::flat_hash_map<std::string, int> prefix_map_t;
+#else
+  typedef std::string_view string_view;
+  typedef std::map<std::string, int, std::less<> > prefix_map_t;
+#endif
 }}
