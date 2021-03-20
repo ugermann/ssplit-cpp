e1fb2ab5f241d8ceccfd00714d5f5e8dd3654f0f
Jerin Philip <jphilip@ed.ac.uk>
2021-03-20 18:57:43 +0000

    if -> ifdef

--- src/ssplit/typedefs.h
+++ src/ssplit/typedefs.h
@@ -1,20 +1,21 @@
 #pragma once
 #include <string>
-#if USE_ABSEIL
-#  include "absl/strings/string_view.h"
-#  include "absl/container/flat_hash_map.h"
-#else
-#  include <map>
-#endif
+#ifdef USE_ABSEIL
+#include "absl/container/flat_hash_map.h"
+#include "absl/strings/string_view.h"
+#else  // USE_ABSEIL
+#include <map>
+#endif  // USE_ABSEIL
 
-namespace ug{
-namespace ssplit{
+namespace ug {
+namespace ssplit {
 
-#if USE_ABSEIL
-  typedef absl::string_view string_view;
-  typedef absl::flat_hash_map<std::string, int> prefix_map_t;
-#else
-  typedef std::string_view string_view;
-  typedef std::map<std::string, int, std::less<> > prefix_map_t;
-#endif
-}}
+#ifdef USE_ABSEIL
+typedef absl::string_view string_view;
+typedef absl::flat_hash_map<std::string, int> prefix_map_t;
+#else   // USE_ABSEIL
+typedef std::string_view string_view;
+typedef std::map<std::string, int, std::less<> > prefix_map_t;
+#endif  //  USE_ABSEIL
+}  // namespace ssplit
+}  // namespace ug
