20cefacab0e82b1aba4844ac799301714160e7c2
Ulrich Germann <ulrich.germann@gmail.com>
2021-04-21 18:51:28 +0100

    Update CMake setup.

--- cmake/ProvideAbseil.cmake
+++ cmake/ProvideAbseil.cmake
@@ -3,11 +3,12 @@ set(ABSEIL_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/src/3rd-party/abseil-cpp")
 
 if (EXISTS ${ABSEIL_SOURCE_DIR}/.git)
   # no need to clone again
+  message("ABSEIL already cloned into ${ABSEIL_SOURCE_DIR}.")
   set(ABSEIL_GIT_REPO "")
 else()
   set(ABSEIL_GIT_REPO "https://github.com/abseil/abseil-cpp.git")
+  message("Will clone ABSEIL from ${ABSEIL_GIT_REPO} into ${ABSEIL_SOURCE_DIR}.")
 endif()
-message("ABSEIL_GIT_REPO is ${ABSEIL_GIT_REPO}")
 
 if(CMAKE_CXX_COMPILER MATCHES "/em\\+\\+(-[a-zA-Z0-9.])?$")
   set(ABSEIL_CMAKE_ARGS_FOR_WASM "-msimd128 -Wno-deprecated-copy-dtor")
@@ -16,7 +17,6 @@ endif()
 ExternalProject_Add(abseil
   PREFIX ${CMAKE_BINARY_DIR}/abseil
   GIT_REPOSITORY ${ABSEIL_GIT_REPO}
-  GIT_TAG "20200923.3"
   GIT_SHALLOW ON
   GIT_PROGRESS OFF
   SOURCE_DIR ${ABSEIL_SOURCE_DIR}
@@ -26,10 +26,9 @@ ExternalProject_Add(abseil
   "-DCMAKE_CXX_FLAGS='${ABSEIL_CMAKE_ARGS_FOR_WASM}'")
 
 include_directories(${CMAKE_BINARY_DIR}/include)
-foreach(alib base city strings hash raw_hash_set)
+foreach(alib base throw_delegate city strings hash raw_hash_set)
   string(CONCAT alibfull
     "${CMAKE_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}"
     "absl_${alib}${CMAKE_STATIC_LIBRARY_SUFFIX}")
-  message("LIBRARY: ${alibfull}")
   set(ABSEIL_LIBS ${ABSEIL_LIBS} ${alibfull})
 endforeach()
