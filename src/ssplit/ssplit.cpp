b36042bc624a44ca58cd741c0f397bea771f51d1
Ulrich Germann <ulrich.germann@gmail.com>
2021-05-04 17:31:46 +0100

    Eliminate abseil dependency; cleanup cmake files.

--- src/ssplit/ssplit.cpp
+++ src/ssplit/ssplit.cpp
@@ -99,8 +99,7 @@ get_prefix_class(string_view piece) const {
   if (foo.consume(&piece, &M, PCRE2_NO_UTF_CHECK) > 0) {
     piece = M[1];
   }
-  std::string key(piece.data(), piece.size());
-  auto m = prefix_type_.find(key);
+  auto m = prefix_type_.find(piece);
   // for debugging:
   // std::cout << piece << " " << (m == prefix_type_.end() ? 0 : m->second) << std::endl;
   return m == prefix_type_.end() ? 0 : m->second;
--- src/ssplit/typedefs.h
+++ src/ssplit/typedefs.h
@@ -2,33 +2,52 @@
 #include <map>
 #include <string>
 
-#if defined(__GNUC__) && !defined(__clang__)
-#ifndef USE_ABSEIL
-#if __GNUC__ < 6 // clang exports GNUC v 4.2
+// We need the extra level of macro redirection to
+// make the preprocessor to expand the macro
+#define CONVERT_TO_STRING_(x) #x
+#define CONVERT_TO_STRING(x) CONVERT_TO_STRING_(x)
+// #define STR2(x) #x
+// #define STR(x) STR2(x)
+
+#pragma message(CONVERT_TO_STRING(__cplusplus))
+// string_view is in experimental/ prior to C++17
+#if __cplusplus <= 201703L
 #include <experimental/string_view>
 #else
 #include <string_view>
 #endif
-#endif
-#endif
 
-#ifdef USE_ABSEIL
-#include "absl/strings/string_view.h"
-#endif  // USE_ABSEIL
+namespace ug {
+namespace ssplit {
+typedef std::experimental::string_view string_view;
+}}
+
+// #if defined(__GNUC__) && !defined(__clang__)
+// #ifndef USE_ABSEIL
+// #if __GNUC__ < 6 // clang exports GNUC v 4.2
+// #include <experimental/string_view>
+// #else
+// #include <string_view>
+// #endif
+// #endif
+// #endif
+
+// #ifdef USE_ABSEIL
+// #include "absl/strings/string_view.h"
+// #endif  // USE_ABSEIL
 
 namespace ug {
 namespace ssplit {
 
-#ifdef USE_ABSEIL
-typedef absl::string_view string_view;
-// USE_ABSEIL
-#elif defined(__GNUC__) && !defined(__clang__) && __GNUC__ < 6
-typedef std::experimental::string_view string_view;
-#else
-typedef std::string_view string_view;
-#endif  //  USE_ABSEIL
+// #ifdef USE_ABSEIL
+// typedef absl::string_view string_view;
+// // USE_ABSEIL
+// #elif defined(__GNUC__) && !defined(__clang__) && __GNUC__ < 6
+// typedef std::experimental::string_view string_view;
+// #else
+// typedef std::string_view string_view;
+// #endif  //  USE_ABSEIL
 
-typedef std::map<std::string, int> prefix_map_t;
+typedef std::map<std::string, int, std::less<>> prefix_map_t;
 }  // namespace ssplit
 }  // namespace ug
-
