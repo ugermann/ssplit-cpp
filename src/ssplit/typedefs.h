1668e1a002b53551f8d80162ee076b50051ff52e
Ulrich Germann <ulrich.germann@gmail.com>
2021-05-04 19:06:14 +0100

    Make things compile with clang.

--- src/ssplit/typedefs.h
+++ src/ssplit/typedefs.h
@@ -6,21 +6,32 @@
 // make the preprocessor to expand the macro
 #define CONVERT_TO_STRING_(x) #x
 #define CONVERT_TO_STRING(x) CONVERT_TO_STRING_(x)
-// #define STR2(x) #x
-// #define STR(x) STR2(x)
 
-#pragma message(CONVERT_TO_STRING(__cplusplus))
-// string_view is in experimental/ prior to C++17
-#if __cplusplus <= 201703L
-#include <experimental/string_view>
+// #pragma message(CONVERT_TO_STRING(__cplusplus))
+#if defined(__GNUC__) && !defined(__clang__) && __GNUC__ < 6
+// use std::experimental::string_view on older versions of gcc
+// clang exports GNUC v 4.2
+#  include <experimental/string_view>
+#elif defined(__clang__) && __cplusplus < 201703L
+// Older versions of cmake don't support CMAKE_CXX_STANDARD 17.
+// On Ubuntu, the corresponding slang version finds string_view here:
+#  include <experimental/string_view>
 #else
-#include <string_view>
+#  include <string_view>
 #endif
 
 namespace ug {
 namespace ssplit {
+#if defined(__GNUC__) && !defined(__clang__) && __GNUC__ < 6
 typedef std::experimental::string_view string_view;
+#elif defined(__clang__) && __cplusplus < 201703L
+typedef std::experimental::string_view string_view;
+#else
+typedef std::string_view string_view;
+#endif
 }}
+
+// #if __GNUC__ < 6 // clang exports GNUC v 4.2
 
 // #if defined(__GNUC__) && !defined(__clang__)
 // #ifndef USE_ABSEIL
@@ -48,6 +59,10 @@ namespace ssplit {
 // typedef std::string_view string_view;
 // #endif  //  USE_ABSEIL
 
+// Before Jerin Philip asks again "What sort of mess is this?"
+// This is C++14 or greater, which allows you to use specify the comparison
+// function (class) for maps explicitly and thus allows you to use string_view
+// directly as keys instead of converting them to std::string first.
 typedef std::map<std::string, int, std::less<>> prefix_map_t;
 }  // namespace ssplit
 }  // namespace ug
